16d34635c79f8c11c94d6065f741e85e5649065a
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-15 10:40:42 +0200

    fix(compound): use hex chain IDs in etherscan base URL map

--- libs/sources/compound/src/calldata/config.ts
+++ libs/sources/compound/src/calldata/config.ts
@@ -9,9 +9,9 @@ export interface CalldataDecoderConfig {
 }
 
 const DEFAULT_BASE_URL_BY_CHAIN_ID: Record<string, string> = {
-  '1': 'https://api.etherscan.io',
-  '137': 'https://api.polygonscan.com',
-  '42161': 'https://api.arbiscan.io',
+  '0x1': 'https://api.etherscan.io',
+  '0x89': 'https://api.polygonscan.com',
+  '0xa4b1': 'https://api.arbiscan.io',
 };
 
 export function readCalldataDecoderConfig(): CalldataDecoderConfig {
