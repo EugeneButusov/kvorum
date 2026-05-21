151826163e9bfa8e0ee915500f1ea13d8b8b7bb6
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-21 10:41:58 +0200

    chore(ci): allowlist public COMP token address for gitleaks

--- libs/sources/compound/src/comp-token/constants.ts
+++ libs/sources/compound/src/comp-token/constants.ts
@@ -3,4 +3,5 @@
 export const COMP_TOKEN_DEPLOY_BLOCK = 9601558;
 
 // Lowercase per existing dao.primary_token_address storage for the same token.
+// gitleaks:allow -- public Ethereum contract address, not a credential.
 export const COMP_TOKEN_ADDRESS = '0xc00e94cb662c3520282e6f5717214004a7f26888';
