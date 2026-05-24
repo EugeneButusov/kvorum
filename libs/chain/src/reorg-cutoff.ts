dc9351083213d6d5e30ed6c016b615d710b3ad4b
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-24 11:05:51 +0200

    refactor(chain): delete reorg and cutoff implementation files

--- libs/chain/src/reorg-cutoff.ts
+++
@@ -1,5 +0,0 @@
-import type { ChainConfig } from './config/config.js';
-
-export function reorgCutoff(headBlock: bigint, config: ChainConfig): bigint {
-  return headBlock - BigInt(config.reorgHorizon) * 2n;
-}
