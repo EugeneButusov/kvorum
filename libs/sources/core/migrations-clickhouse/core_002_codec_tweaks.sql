e652dfe068443e6a0df6c4fcc1c7738f6995b1a2
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-26 11:18:50 +0200

    fix(ch-migration): remove unsupported T64 codec for UInt256

--- libs/sources/core/migrations-clickhouse/core_002_codec_tweaks.sql
+++ libs/sources/core/migrations-clickhouse/core_002_codec_tweaks.sql
@@ -1,8 +1,8 @@
 ALTER TABLE vote_events_analytics
-    MODIFY COLUMN voting_power UInt256 CODEC(T64, ZSTD(1));
+    MODIFY COLUMN voting_power UInt256 CODEC(ZSTD(1));
 
 ALTER TABLE delegation_flow_analytics
-    MODIFY COLUMN voting_power UInt256 CODEC(T64, ZSTD(1));
+    MODIFY COLUMN voting_power UInt256 CODEC(ZSTD(1));
 
 ALTER TABLE vote_events_analytics
     MODIFY COLUMN block_number UInt64 CODEC(DoubleDelta, ZSTD(1));
