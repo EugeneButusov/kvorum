e63b9ffb356ee1400e92befe7cf152e6fea06647
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-27 23:20:58 +0200

    refactor(delegations): use DbModule providers instead of direct db clients

--- nest/delegations/src/delegations.module.ts
+++ nest/delegations/src/delegations.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DaoReadRepository, DelegationReadRepository, chDb, pgDb } from '@libs/db';
+import { DaoReadRepository, DelegationReadRepository } from '@libs/db';
+import { DbModule } from '@nest/db';
 
 @Module({
-  providers: [
-    {
-      provide: DelegationReadRepository,
-      useFactory: () => new DelegationReadRepository(pgDb, chDb as never),
-    },
-    { provide: DaoReadRepository, useFactory: () => new DaoReadRepository(pgDb) },
-  ],
+  imports: [DbModule.forFeature([DelegationReadRepository, DaoReadRepository])],
   exports: [DelegationReadRepository, DaoReadRepository],
 })
 export class DelegationsModule {}
