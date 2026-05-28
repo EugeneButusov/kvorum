1b47cce79755b7002122fc3e84efa91d895cbb7b
Eugene Butusov <evgenybutusov@gmail.com>
2026-05-28 09:43:24 +0200

    chore(nest): add TODO for db module repo re-export cleanup

--- nest/delegations/src/delegations.module.ts
+++ nest/delegations/src/delegations.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { DaoReadRepository, DelegationReadRepository } from '@libs/db';
 import { DbModule } from '@nest/db';
 
+// TODO(tech-debt): replace this dynamic-module re-export pattern with explicit
+// repository provider wiring once the shared db module API is cleaned up.
 const DELEGATIONS_DB_MODULE = DbModule.forFeature([DelegationReadRepository, DaoReadRepository]);
 
 @Module({
