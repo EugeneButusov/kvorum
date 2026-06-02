d85b156b5d32ea37e1b6a0f88f7eb1bb5860b903
Eugene Butusov <evgenybutusov@gmail.com>
2026-06-02 14:43:10 +0200

    refactor(aave): drop re-exports of @sources/core primitives from public index

--- libs/sources/aave/src/index.ts
+++ libs/sources/aave/src/index.ts
@@ -11,10 +11,3 @@ export type {
 } from './persistence/schema';
 
 export * from './governance-v3';
-export { DecodeError } from '@sources/core';
-export type {
-  ArchiveWriteContext,
-  ArchiveWriteOutcome,
-  DecodeErrorReason,
-  IngesterListenerOptions,
-} from '@sources/core';
